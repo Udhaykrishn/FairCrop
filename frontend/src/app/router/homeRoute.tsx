62b48268fef7c75851ddac42cea1c92d42dfb278
kpmisthah <kpmisthah6@gmail.com>
2026-02-27 23:25:07 +0530

    fix:delete homeRoute

--- frontend/src/app/router/homeRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoute } from '@tanstack/react-router'
-import { rootRoute } from './rootRoute'
-import { LandingPage } from '@/features/home/pages/LandingPage'
-
-export const homeRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: '/',
-    component: LandingPage,
-})
